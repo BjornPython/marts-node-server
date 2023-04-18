9aacb838ebfed566d49c3881120b4125d994251d
Nathan <nathanflores887@gmail.com>
2023-04-18 13:20:47 +0800

    removed console logs

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const martialArts = require("./data/marts.json")
 const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-    console.log("METHODS: ", res.meth);
     res.writeHead(200, {"Content-Type": "application/json"})
     res.end(JSON.stringify(martialArts))
 })
