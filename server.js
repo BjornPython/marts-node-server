3e12c073d97549e88ed7d566628c3e096c881189
Nathan <nathanflores887@gmail.com>
2023-04-24 17:42:49 +0800

    doc(server.js): add documentations for each routes

--- server.js
+++ server.js
@@ -9,21 +9,25 @@ const { handleUiRequest, handleScriptRequest, handleStylesRequest, handlePageNot
 const { handleCreateRequest, handleReadRequest, handleUpdateRequest, handleDeleteRequest } = require("./routes/martial-arts.route.js")
 const server = http.createServer(async (req, res) => {
 
+    // Route for serving html
     if (req.url === "/martial-arts" && req.method === "GET") { handleUiRequest(req, res) }
 
+    // Route for CREATING a new martial art
     else if (req.url === "/martial-arts" && req.method === "POST") { handleCreateRequest(req, res) }
 
-    else if (req.url === "/delete" && req.method === "DELETE") { handleDeleteRequest(req, res) }
-
-    else if (req.url === "/update" && req.method === "PATCH") { handleUpdateRequest(req, res) }
-
-    // Route for getting client.js file
-    else if (req.url === "/client.js") { handleScriptRequest(req, res) }
-
-    // Route for getting currentData
+    // Route for READING current martial arts
     else if (req.url === "/data") { handleReadRequest(req, res) }
 
-    // Route for getting styles.css file
+    // Route for UPDATING a martial art
+    else if (req.url === "/update" && req.method === "PATCH") { handleUpdateRequest(req, res) }
+
+    // Route for DELETING a martial art
+    else if (req.url === "/delete" && req.method === "DELETE") { handleDeleteRequest(req, res) }
+
+    // Route for serving the client.js file
+    else if (req.url === "/client.js") { handleScriptRequest(req, res) }
+
+    // Route for serving the styles.css file
     else if (req.url === "/styles.css") { handleStylesRequest(req, res) }
 
     else { handlePageNotFound(req, res) }
